7aeecea6a1865ddfb537a1f08f847321bdc4adf6
Ejiro Umaka <ejironaomiumaka@gmail.com>
2022-05-27 10:35:12 -0400

    Add files via upload

---
+++ Macros/Fun4All_HCALO_sPHENIX.C
@@ -0,0 +1,190 @@
+#ifndef MACRO_FUN4ALLG4SPHENIX_C
+#define MACRO_FUN4ALLG4SPHENIX_C
+
+#include <GlobalVariables.C>
+
+#include <DisplayOn.C>
+#include <G4Setup_sPHENIX.C>
+#include <G4_Bbc.C>
+#include <G4_CaloTrigger.C>
+#include <G4_Centrality.C>
+#include <G4_DSTReader.C>
+#include <G4_Global.C>
+#include <G4_HIJetReco.C>
+#include <G4_Input.C>
+#include <G4_Jets.C>
+#include <G4_KFParticle.C>
+#include <G4_ParticleFlow.C>
+#include <G4_Production.C>
+#include <G4_TopoClusterReco.C>
+#include <G4_Tracking.C>
+#include <G4_User.C>
+#include <QA.C>
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+
+// For HepMC Hijing
+// try inputFile = /sphenix/sim/sim01/sphnxpro/sHijing_HepMC/sHijing_0-12fm.dat
+
+int Fun4All_HCALO_sPHENIX(
+    const int nEvents = 1,
+    const string &inputFile = "",
+    const string &outputFile = "",
+    const int skip = 0,
+    const string &outdir = ".")
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(0);
+  PHRandomSeed::Verbosity(1);
+  Input::VERBOSITY = 0;
+
+  //===============
+  // Input options
+  //===============
+    
+  Input::SIMPLE = false;
+  Input::GUN = true;
+    
+  // This creates the input generator(s)
+  InputInit();
+
+  if (Input::SIMPLE)
+  {
+     INPUTGENERATOR::SimpleEventGenerator[0]->add_particles("pi-", 5);
+
+     INPUTGENERATOR::SimpleEventGenerator[0]->set_vertex_distribution_function(PHG4SimpleEventGenerator::Gaus,
+                                                                                PHG4SimpleEventGenerator::Gaus,
+                                                                                PHG4SimpleEventGenerator::Gaus);
+     INPUTGENERATOR::SimpleEventGenerator[0]->set_vertex_distribution_mean(0., 0., 0.);
+     INPUTGENERATOR::SimpleEventGenerator[0]->set_vertex_distribution_width(0.01, 0.01, 5.);
+    
+     INPUTGENERATOR::SimpleEventGenerator[0]->set_eta_range(-1, 1);
+     INPUTGENERATOR::SimpleEventGenerator[0]->set_phi_range(-M_PI, M_PI);
+     INPUTGENERATOR::SimpleEventGenerator[0]->set_pt_range(0.1, 20.);
+  }
+ 
+  if (Input::GUN)
+  {
+    INPUTGENERATOR::Gun[0]->AddParticle("pi-", 0, 1, 0);
+    INPUTGENERATOR::Gun[0]->set_vtx(0, 0, 0);
+  }
+
+  // register all input generators with Fun4All
+  InputRegister();
+
+  // turn the display on (default off)
+  Enable::DISPLAY = true;
+
+  
+  Enable::HCALOUT = true;
+  Enable::HCALOUT_ABSORBER = true;
+  Enable::HCALOUT_CELL = Enable::HCALOUT && true;
+  Enable::HCALOUT_TOWER = Enable::HCALOUT_CELL && true;
+  Enable::HCALOUT_CLUSTER = Enable::HCALOUT_TOWER && true;
+  Enable::HCALOUT_EVAL = Enable::HCALOUT_CLUSTER && true;
+  Enable::HCALOUT_QA = Enable::HCALOUT_CLUSTER && Enable::QA && true;
+
+  // Initialize the selected subsystems
+  G4Init();
+
+  //---------------------
+  // GEANT4 Detector description
+  //---------------------
+  if (!Input::READHITS)
+  {
+    G4Setup();
+  }
+
+
+  if (Enable::HCALOUT_CELL) HCALOuter_Cells();
+
+  //-----------------------------
+  // HCAL towering and clustering
+  //-----------------------------
+
+
+  if (Enable::HCALOUT_TOWER) HCALOuter_Towers();
+  if (Enable::HCALOUT_CLUSTER) HCALOuter_Clusters();
+
+  //----------------------
+  // Simulation evaluation
+  //----------------------
+  string outputroot = outputFile;
+  string remove_this = ".root";
+  size_t pos = outputroot.find(remove_this);
+  if (pos != string::npos)
+  {
+    outputroot.erase(pos, remove_this.length());
+  }
+
+   //--------------
+  // Set up Input Managers
+  //--------------
+
+  InputManagers();
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+
+  //-----------------
+  // Event processing
+  //-----------------
+  if (Enable::DISPLAY)
+  {
+    DisplayOn();
+
+    gROOT->ProcessLine("Fun4AllServer *se = Fun4AllServer::instance();");
+    gROOT->ProcessLine("PHG4Reco *g4 = (PHG4Reco *) se->getSubsysReco(\"PHG4RECO\");");
+
+    cout << "-------------------------------------------------" << endl;
+    cout << "You are in event display mode. Run one event with" << endl;
+    cout << "se->run(1)" << endl;
+    cout << "Run Geant4 command with following examples" << endl;
+    gROOT->ProcessLine("displaycmd()");
+
+    return 0;
+  }
+
+  // if we use a negative number of events we go back to the command line here
+  if (nEvents < 0)
+  {
+    return 0;
+  }
+  // if we run the particle generator and use 0 it'll run forever
+  // for embedding it runs forever if the repeat flag is set
+  if (nEvents == 0 && !Input::HEPMC && !Input::READHITS && INPUTEMBED::REPEAT)
+  {
+    cout << "using 0 for number of events is a bad idea when using particle generators" << endl;
+    cout << "it will run forever, so I just return without running anything" << endl;
+    return 0;
+  }
+
+  se->skip(skip);
+  se->run(nEvents);
+
+
+  //-----
+  // Exit
+  //-----
+
+  se->End();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+
+  gSystem->Exit(0);
+  return 0;
+}
+#endif
